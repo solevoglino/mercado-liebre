86e0f3f5f41b421d672fbf5357ae237e5a68ba92
Soledad <soledad@MacBook-Pro-de-Soledad.local>
2023-05-09 13:16:25 -0300

    subo modific. buscador y formularios

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("servidor corriendo ");
 })
 
+
+
 app.get("/" , (req, res)=> {
-    res.sendFile(__dirname + "/views/home.html" )
+    res.sendFile(__dirname + "/views/home.html" );
 })
+
+app.get("/register" , (req, res)=> {
+    res.sendFile(__dirname + "/views/register.html" );
+})
+
+app.get("/login" , (req, res)=> {
+    res.sendFile(__dirname + "/views/login.html" );
+})
+
